0a446ce3adae6b3bf03f67b73a4d33aab1c4fe46
Amit Gupta <ami2019hi@gmail.com>
2025-10-24 13:12:28 +0530

    calculate area

---
+++ java/area.java
@@ -0,0 +1,13 @@
+public class area {
+    public static void main(String[] args) {
+        double pi;
+        double r;
+        double a;
+        pi = 3.1416;
+        r = 100;
+        a = pi * r * r;
+        System.out.print("the area of circle with radius " + r);
+        System.out.println(" is =" + a);
+
+    }
+}
