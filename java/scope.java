b7341425c6fe24e001b8d10c96e0f824e571e187
Amit Gupta <ami2019hi@gmail.com>
2025-10-24 21:10:03 +0530

    scope modified

--- java/scope.java
+++ java/scope.java
@@ -1,12 +1,18 @@
 public class scope {
     public static void main(String[] args) {
         int X;
-        X=10;
-        if (X==10){
-         int   Y=20;
-            System.out.println("x and y :" +X + " " + Y);
-            X=Y*2;
+        X = 10;
+        if (X == 10) {
+            int Y = 20;
+            System.out.println("x and y :" + X + " " + Y);
+            X = Y * 2;
         }
-        System.out.println(" x is :"+X);
+        System.out.println(" x is :" + X);
+        for (X = 0; X < 4; X++) {
+            int y = -1;
+            System.out.println("y is :" + y);
+            y = 100;
+            System.out.println(" y is now :" + y);
+        }
     }
 }
