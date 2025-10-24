81d0d26f9c81688323a207f5d0e635690cba55c6
Amit Gupta <ami2019hi@gmail.com>
2025-10-24 13:25:50 +0530

    charDemo

---
+++ java/charDemo.java
@@ -0,0 +1,16 @@
+public class charDemo {
+    public static void main(String[] args) {
+        char ch1,ch2;
+        ch1 = 88; //code for X
+        ch2 = 'Y';
+        System.out.print( "ch1 and ch2 : ");
+        System.out.println(ch1+ " " +ch2);
+
+        //now the incrememnt is added to both characters
+        ch1++;
+        ch2++;
+        System.out.print("now the ch1 and ch2 are incremented : ");
+        System.out.println(ch1+ " " +ch2);
+
+    }
+}
