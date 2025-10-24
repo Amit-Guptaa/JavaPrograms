724f80ce6689f34a66dcedb915b201853c512f7d
Amit Gupta <ami2019hi@gmail.com>
2025-10-24 20:32:07 +0530

    dynamic initialiazation

---
+++ java/DynInit.java
@@ -0,0 +1,13 @@
+public class DynInit {
+    // Demonstrating Dynamic initialization
+    public static void main(String[] args) {
+        double a;
+        double b;
+        a = 3;
+        b = 4;
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("hypotenuse is " + c);
+
+    }
+
+}
