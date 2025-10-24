5f1508ec9c1ad7b6fc0824a80e8a2e10b6a17913
Amit Gupta <ami2019hi@gmail.com>
2025-10-24 19:01:50 +0530

    boolean test

---
+++ java/booleanTest.java
@@ -0,0 +1,15 @@
+public class booleanTest {
+    public static void main(String[] args) {
+        boolean b;
+        b=true;
+        System.out.println("b is " +b);
+        b=false;
+        System.out.println("b is " +b);
+        if(b)
+        System.out.println("this is executable");
+        b=true;
+        if(b)
+        System.out.println("this is not executable");
+        System.out.println("(10>9) is " +(10>9));
+    }
+}
