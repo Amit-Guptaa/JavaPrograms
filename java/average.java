c857581ebe155dd99e392eb6f035df471bf38a35
Amit Gupta <ami2019hi@gmail.com>
2025-10-25 08:21:57 +0530

    about array

---
+++ java/average.java
@@ -0,0 +1,13 @@
+public class average {
+    public static void main(String[] args) {
+        double nums [] = {56.4,78.4,23.7,45.8,56.5};
+        double result=0;
+        int i;    
+        for (i=0;i<5;i++){
+            result=result+nums[i];
+        }
+            System.out.println("Average is :"+result/5);
+
+        
+      }
+}
