61bc05b2d91263a67d17ee1683c91f7b7842bf05
Amit Gupta <ami2019hi@gmail.com>
2025-10-29 20:20:04 +0530

    modified

--- javaarray/threeDmatrix.java
+++ javaarray/threeDmatrix.java
@@ -4,13 +4,15 @@ public class threeDmatrix {
     public static void main(String[] args) {
         int[][][] threeD= new int[3][4][5];
         int i,j,k;
-        for(i=0;i<3;i++)
-            for(j=0;j<4;j++)
-                for(k=0;k<4;k++)
+        for(i=0;i<3;i++){
+            for(j=0;j<4;j++){
+                for(k=0;k<4;k++){
                 
                     threeD[i][j][k]= i*j*k;
             
-                
+            }
+        }
+    }
             
         
         for(i=0;i<3;i++){
