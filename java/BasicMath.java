a12035b83b78ce4fbfc135c3c6ce81d4bd0cfa7d
Amit Gupta <ami2019hi@gmail.com>
2025-10-31 12:25:55 +0530

    algebra

---
+++ java/BasicMath.java
@@ -0,0 +1,37 @@
+public class BasicMath {
+    public static void main(String[] args) {
+         int a, b, c, d, e;
+        a = 5 + 3;
+        b = a - 4;
+        c = b * 5;
+        d = b - c;
+        e = a / b;
+        System.out.println("integers value");
+        System.out.println("value of a = " + a);
+        System.out.println("value of b = " + b);
+        System.out.println("value of c = " + c);
+        System.out.println("value of d = " + d);
+        System.out.println("value of e = " + e);
+        // algebra using doubles
+        double da, db, dc, dd, de;
+        da = 5 + 8;
+        db = a - 9.8;
+        dc = db * da;
+        dd = dc % 5;
+        de = dd / da;
+        System.out.println("doubles value.");
+        System.out.println("value of da = " + da);
+
+        System.out.println("value of db = " + db);
+
+        System.out.println("value of dc = " + dc);
+
+        System.out.println("value of dd = " + dd);
+
+        System.out.println("value of de = " + de);
+
+    }
+}
+
+    
+
