335def1cce0d30a2313cf8d05b0fedca09266576
Amit Gupta <ami2019hi@gmail.com>
2025-11-05 23:15:45 +0530

    added modulus program

---
+++ newjava/modulus.java
@@ -0,0 +1,10 @@
+package newjava;
+
+public class modulus {
+    public static void main(String[] args) {
+        int x=10;
+        double y=56.90;
+        System.out.println("x mod 3 is " +x%3);
+        System.out.println("y mod 10 is "+ y%10);
+    }
+}
