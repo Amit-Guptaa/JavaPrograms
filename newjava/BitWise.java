28bdaa665b03395804dcdcc7469389283215f482
Amit Gupta <ami2019hi@gmail.com>
2025-11-07 01:04:28 +0530

    Bitwise operators

---
+++ newjava/BitWise.java
@@ -0,0 +1,12 @@
+package newjava;
+
+public class BitWise {
+    public static void main(String[] args) {
+        int a=6;
+        int b=7;
+        System.out.println("AND " +(a&b));
+        System.out.println("OR " +(a|b));
+        System.out.println("XOR " +(a^b));
+        System.out.println("complement " +(~a));
+    }
+}
