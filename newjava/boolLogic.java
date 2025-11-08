f3c573d896777974a7ec0350bbf4ae85e3e2321e
Amit Gupta <ami2019hi@gmail.com>
2025-11-09 01:30:34 +0530

    boolean logic

---
+++ newjava/boolLogic.java
@@ -0,0 +1,22 @@
+package newjava;
+
+public class boolLogic {
+    public static void main(String[] args) {
+        boolean a= true;
+        boolean b=false;
+        boolean c=a|b;
+        boolean d=a&b;
+        boolean e= a^b;
+        boolean f= !a&b|a&!b;
+         boolean g= !a;
+         boolean h= !b;
+         System.out.println( "a = "+a);
+         System.out.println( "b = "+b);
+         System.out.println( "a|b = "+c);
+         System.out.println( "a&b = "+d);
+         System.out.println( "a^b = "+e);
+         System.out.println( "!a&b|a&!b = "+f);
+         System.out.println( "!a = "+g);
+         System.out.println( "!b = "+h);
+    }
+}
