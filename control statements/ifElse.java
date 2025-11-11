7ad2fbd45db211ca4d71eae456e9d1d129242d9a
Amit Gupta <ami2019hi@gmail.com>
2025-11-11 23:39:17 +0530

    user input method

--- control statements/ifElse.java
+++ control statements/ifElse.java
@@ -1,20 +1,27 @@
+import java.util.Scanner;
 
 public class ifElse {
     public static void main(String[] args) {
-        int month=6;
+        Scanner scanner = new Scanner(System.in);
+        int month;
+        System.out.println("enter the month in number : ");
+        month = scanner.nextInt();
         String season;
-        if(month==11||month==12||month==1||month==2)
+
+        if (month == 11 || month == 12 || month == 1 || month == 2)
             season = "winter";
-        else if( month==3||month==4)
-            season = "spring"; 
-        
-        else if ( month==5||month==6||month==7)
-                season = "summer";
-        
-                else 
-                    season = "unknown";     
-            
-        System.out.println("june is a month of " +season);
-        
+        else if (month == 3 || month == 4)
+            season = "spring";
+
+        else if (month == 5 || month == 6 || month == 7)
+            season = "summer";
+
+        else
+            season = "unknown";
+
+        System.out.println("this month has " +season+ " season.");
+
+        scanner.close();
+
     }
 }
