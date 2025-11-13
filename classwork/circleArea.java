a9516b34e83cbb3ef442a6cb5d12e542d3720128
Amit Gupta <ami2019hi@gmail.com>
2025-11-13 17:48:47 +0530

    circle:

--- classwork/circleArea.java
+++ classwork/circleArea.java
@@ -1,26 +1,36 @@
-class circle{
-    float radius,area;
-    void setRadius(float r){
-        radius=r;
+import java.util.Scanner;
+
+class circle {
+    float radius, area;
+
+    void setRadius(float r) {
+        radius = r;
     }
-    float getradius(){
-        return(radius);
+
+    float getradius() {
+        return (radius);
     }
-    void setArea(float a){
-        area=a;
+
+    void setArea(float a) {
+        area = a;
     }
-    float getArea(){
-        area=3.14f*radius*radius;
-        return(area);
-        
+
+    float getArea() {
+        area = 3.14f * radius * radius;
+        return (area);
+
     }
 
 }
 
 public class circleArea {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         circle a1 = new circle();
-        a1.setRadius(100);
-        System.out.println("area of circle whose radius is " +a1.getradius()+ " will be calculated to "+a1.getArea());
+        System.out.println("enter the radius:");
+        a1.setRadius(sc.nextFloat());
+        System.out
+                .println("area of circle whose radius is " + a1.getradius() + " will be calculated to " + a1.getArea());
+        sc.close();
     }
 }
