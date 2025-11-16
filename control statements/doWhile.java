d030a6f216b7f04b2eac033ed53a2d23084c5c48
Amit Gupta <ami2019hi@gmail.com>
2025-11-16 16:11:42 +0530

    DoWhile loop

--- control statements/doWhile.java
+++ control statements/doWhile.java
@@ -1,8 +1,12 @@
 public class doWhile {
     public static void main(String[] args) {
         int n=10;
-        do()
+        do{
+            System.out.println(" tik tik tik "+n);
+        n--;
+        } while (n>0);
+        
+ 
+            }
 
-        }
-    
 }
