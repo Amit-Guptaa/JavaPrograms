5ca1d9e4b110f0e6140c8720b776c1df01907cd6
Amit Gupta <ami2019hi@gmail.com>
2025-11-16 15:56:20 +0530

    mid

--- control statements/While.java
+++ control statements/While.java
@@ -1,6 +1,11 @@
 public class While {
     public static void main(String[] args) {
-        int n=10;
+        int n=10; int i,j;
+        i=100;
+        j=200;
+        while (i++<--j) {
+            System.out.println("midpoint is :"+i);
+        }
         while (n>0) 
         {
             System.out.println(" n is "+n);
---
+++ control statements/doWhile.java
@@ -0,0 +1,8 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int n=10;
+        do()
+
+        }
+    
+}
