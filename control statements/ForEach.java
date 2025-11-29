5e1db1e3dee559e18abec41c9bde92bc5f63e9db
Amit Gupta <ami2019hi@gmail.com>
2025-11-30 03:36:07 +0530

    modified

--- control statements/ForEach.java
+++ control statements/ForEach.java
@@ -2,6 +2,7 @@ public class ForEach {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int sum = 0;
+        int sums=0;
         for (int x : nums) {
             System.out.println("value is: " + x);
             sum += x;
@@ -12,14 +13,15 @@ public class ForEach {
 
         for (int y : nums) {
             System.out.println("value is: " + y);
-            sum += y;
+            sums += y;
             if (y == 5) {
                 break;
             }
         }
-        System.out.println("================");
+        System.out.println("================"); 
+        System.out.println("Sum of the first five natural number is: ");
 
-        System.out.println("summation: " + sum);
+        System.out.println("summation: " + sums);
         System.out.println("================");
 
     }
